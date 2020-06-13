6febdd522b6f02655649ba5ae9ff9bc2d2948526
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-06-13 21:12:45 +0200

    host fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 export class AppConfig {
   //static ApiBaseURL = "https://localhost:9001/api/";
-  static ApiBaseURL = "https://kwetter.basvdeertwegh.nl/api/";
+  static ApiBaseURL = "http://kwetter.basvdeertwegh.nl/api/";
+  //static ApiBaseURL = "http://35.233.59.167/api/";
 
   static ApiUrls = {
     LOGIN: "auth",
